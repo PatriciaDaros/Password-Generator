eda0699230d44619f80544ffafbf95f9707ad5a3
RuanMiniguite <ruanminiguite@gmail.com>
2022-05-02 21:30:48 -0300

    :package: Screen creation

---
+++ Generator.py
@@ -0,0 +1,20 @@
+from random import choice
+import string
+from time import sleep
+from tkinter import *
+import _thread
+
+bg = "#FFF"
+
+
+root = Tk()
+root.title("Generator")
+root.geometry("400x300+200+200")
+root.resizable(False, False)
+root.iconbitmap("Icon.ico")
+root.config(bg=bg)
+
+
+
+
+root.mainloop()
